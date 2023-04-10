23810b88efb7223b695e4eaa49b399a1d9cb844c
André Filipe <eutech.backup@gmail.com>
2023-04-10 18:14:19 -0300

    Updated database connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,11 +2,11 @@ const pg = require('pg')
 
 function dbConnection() {
   return new pg.Client({
-    host: 'localhost',
+    host: 'dpg-cgq7k3m4dadce8312qcg-a',
     port: 5432,
-    database: 'notes_app',
-    user: 'postgres',
-    password: 'admin'
+    database: 'notes_app_kblp',
+    user: 'notes_app_kblp_user',
+    password: 'H2EboA2KxhGqeadrsqYwbXaAsOzKNQhj'
   })
 }
 
