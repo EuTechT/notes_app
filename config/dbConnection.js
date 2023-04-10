68957fbf763a2581efae515cc18acdbbd993c390
André Filipe <eutech.backup@gmail.com>
2023-04-10 19:11:03 -0300

    Updated database connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,15 @@
 const pg = require('pg')
 
 function dbConnection() {
-  return new pg.Client({
-    host: 'dpg-cgq7k3m4dadce8312qcg-a',
-    port: 5432,
-    database: 'notes_app_kblp',
-    user: 'notes_app_kblp_user',
-    password: 'H2EboA2KxhGqeadrsqYwbXaAsOzKNQhj'
-  })
+  // return new pg.Client({
+  //   host: 'dpg-cgq7k3m4dadce8312qcg-a',
+  //   port: 5432,
+  //   database: 'notes_app_kblp',
+  //   user: 'notes_app_kblp_user',
+  //   password: 'H2EboA2KxhGqeadrsqYwbXaAsOzKNQhj'
+  // })
+
+  new pg.Client('postgres://notes_app_kblp_user:H2EboA2KxhGqeadrsqYwbXaAsOzKNQhj@dpg-cgq7k3m4dadce8312qcg-a/notes_app_kblp')
 }
 
 module.exports = () => dbConnection
